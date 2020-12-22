7085e73d6b37ea8f19eec2540719ca85d80825ff
José Luis Lafuente <jl@lafuente.me>
2020-12-22 22:45:30 +0100

    jdt-ls: init at 0.67.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22211,6 +22211,8 @@ in
 
   iwona = callPackage ../data/fonts/iwona { };
 
+  jdt-ls = callPackage ../development/tools/jdt-ls { };
+
   jetbrains-mono = callPackage ../data/fonts/jetbrains-mono { };
 
   jost = callPackage ../data/fonts/jost { };
